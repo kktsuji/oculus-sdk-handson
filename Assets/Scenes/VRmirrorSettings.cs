3fd00ad346e79a407b4d159455b36e576b1391ab
Kouki Tsuji <cookieee.626+tech@gmail.com>
2020-07-24 00:49:04 +0900

    Add Unitychan and Mirror

---
+++ Assets/Scenes/VRmirrorSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRmirrorSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.XR.XRSettings.showDeviceView = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
